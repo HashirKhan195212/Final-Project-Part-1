2276486159217abb9dce944057c07f895c622e7b
HashirKhan195212 <hashir20051234@gmail.com>
2024-11-12 19:14:36 -0500

    removed some items from index and added index and comments

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,25 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/home', function(req, res, next) {
+/* GET index page. */
+router.get('Calculate/index', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
-/* GET about us page. */
-router.get('/aboutus', function(req, res, next) {
-  res.render('index', { title: 'About Us' });
-});
-/* GET product page. */
-router.get('/product', function(req, res, next) {
-  res.render('index', { title: 'Product' });
-});
-/* GET services page. */
-router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
-});
-/* GET contact us page. */
-router.get('/contactus', function(req, res, next) {
-  res.render('index', { title: 'Contact Us' });
-});
 
+// exports router
 module.exports = router;
